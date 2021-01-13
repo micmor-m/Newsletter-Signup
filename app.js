32b7a91c1d989d4f04c16e0d8b02b5e3d7106b1e
mic <micmor.m@gmail.com>
2021-01-12 22:14:27 -0500

    finalized POST request to mailchimp API

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const path = require("path");
+const { response } = require("express");
+const { url } = require("inspector");
+
+//require dotenv to import env variable
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,9 +21,43 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body.firstName);
-  console.log(req.body.lastName);
-  console.log(req.body.email);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  //the url is provided by the API owner
+  const url =
+    "https://us7.api.mailchimp.com/3.0/lists/" + `${process.env.LIST_ID}`;
+
+  const options = {
+    method: "POST",
+    auth: `user:${process.env.API_KEY}`,
+  };
+
+  //Method request of https allow to specify the method of the request (GET, POST,...)
+  const request = https.request(url, options, (response) => {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, () => {
